e84ca93787ba01be62a83f056882a70d594a3451
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2017-04-09 03:01:47 -0300

    Updated combineResolver to always return a value (not throw errors).

--- src/combineResolvers.js
+++ src/combineResolvers.js
@@ -15,4 +15,4 @@ export const combineResolvers = (...funcs) => (...args) => funcs.reduce(
   (prevPromise, resolver) => prevPromise.then(
     prev => prev === skip ? resolver(...args) : prev)
   , Promise.resolve()
-)
+).catch(err => err)
