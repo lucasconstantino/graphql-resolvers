8edc9669617ef6b2e54ebf562f2583a4d09278ce
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2017-05-07 20:50:43 -0300

    Fixed pipeResolvers export

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 
 module.exports.skip = require('./lib/utils.js').skip
 module.exports.combineResolvers = require('./lib/combineResolvers.js').combineResolvers
-module.exports.pipeResolvers = require('./lib/pipeResolvers.js').combineResolvers
+module.exports.pipeResolvers = require('./lib/pipeResolvers.js').pipeResolvers
