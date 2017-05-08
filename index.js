2d4cea58e2d624f3fc07d9e9741f8fbc58fd4015
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2017-05-08 00:30:15 -0300

    Added new dependencies.

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 module.exports.skip = require('./lib/utils.js').skip
 module.exports.combineResolvers = require('./lib/combineResolvers.js').combineResolvers
 module.exports.pipeResolvers = require('./lib/pipeResolvers.js').pipeResolvers
+module.exports.dependentResolver = require('./lib/dependingResolvers.js').dependentResolver
+module.exports.dependeeResolver = require('./lib/dependingResolvers.js').dependeeResolver
