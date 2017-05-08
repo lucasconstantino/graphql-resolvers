677b2b5c8f0dce0f0498e1f3051e63850e4974be
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2017-05-08 16:47:01 -0300

    Added composition utilities aliases.

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ module.exports.allResolvers = require('./lib/allResolvers.js').allResolvers
 module.exports.pipeResolvers = require('./lib/pipeResolvers.js').pipeResolvers
 module.exports.combineResolvers = require('./lib/combineResolvers.js').combineResolvers
 
+// Aliases.
+module.exports.all = module.exports.allResolvers
+module.exports.pipe = module.exports.pipeResolvers
+module.exports.combine = module.exports.combineResolvers
+
 // Field dependency.
 module.exports.isDependee = require('./lib/dependingResolvers.js').isDependee
 module.exports.dependeeResolver = require('./lib/dependingResolvers.js').dependeeResolver
