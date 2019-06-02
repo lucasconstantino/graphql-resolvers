b32351b9d9798d8a186bdcb8b4436c0c44566190
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-06-02 17:39:28 -0300

    refactor: updated util test forr jest

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,17 +1,16 @@
-import { expect } from 'chai'
 import { skip, nextTick } from '../src/utils'
 
 describe('utils', () => {
   describe('skip', () => {
     it('should equal undefined', () => {
-      expect(skip).to.equal(undefined)
+      expect(skip).toEqual(undefined)
     })
   })
 
   describe('nextTick', () => {
     it('should return a promised version of a value', async () => {
       const value = await nextTick(1)
-      expect(value).to.equal(1)
+      expect(value).toEqual(1)
     })
   })
 })
