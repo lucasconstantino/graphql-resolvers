e34755841676ccc1f7c36aa178663ac473e45dc7
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-06-02 17:19:51 -0300

    refactor: updated miscResolvers test for Jest

--- test/miscResolvers.test.js
+++ test/miscResolvers.test.js
@@ -1,41 +1,37 @@
 // import { last, length, groupBy, pipe, prop, sortBy, values } from 'ramda'
-import chai, { expect } from 'chai'
-import spies from 'chai-spies'
-
 import { skip } from '../src/utils'
 import { loggingResolver, contextMustBeObject } from '../src/miscResolvers'
 
-// Apply chai extensions.
-chai.use(spies)
-
 describe('miscResolvers', () => {
   describe('loggingResolver', () => {
-    const backup = console.log
-
     beforeEach(() => {
-      console.log = chai.spy()
+      jest.spyOn(console, 'log').mockImplementation(() => {})
     })
 
     afterEach(() => {
-      console.log.reset()
-      console.log = backup
+      console.log.mockRestore()
     })
 
     it('should log any passed arguments', () => {
       loggingResolver('foo', 'bar', 'baz')
-      expect(console.log).to.have.been.called.once.with(['foo', 'bar', 'baz'])
+
+      expect(console.log).toHaveBeenCalledTimes(1)
+      expect(console.log).toHaveBeenCalledWith(
+        ['foo', 'bar', 'baz'],
+        expect.anything()
+      )
     })
 
     it('should return a skip', () => {
-      expect(loggingResolver()).to.equal(skip)
+      expect(loggingResolver()).toEqual(skip)
     })
   })
 
   describe('contextMustBeObject', () => {
     it('should return error when no arguments is suplied', () => {
       expect(contextMustBeObject())
-        .to.be.an('error')
-        .and.have.property(
+        .toBeInstanceOf(Error)
+        .toHaveProperty(
           'message',
           'Some functionality requires context to be an object.'
         )
@@ -43,21 +39,20 @@ describe('miscResolvers', () => {
 
     it('should return error when 3th argument (context) is not an object', () => {
       expect(contextMustBeObject(null, null, 2))
-        .to.be.an('error')
-        .and.have.property(
+        .toBeInstanceOf(Error)
+        .toHaveProperty(
           'message',
           'Some functionality requires context to be an object.'
         )
     })
 
     it('should return skip when 3th argument (context) is an object of any kind', () => {
-      expect(contextMustBeObject(null, null, {})).to.be.equal(skip)
-      expect(contextMustBeObject(null, null, [])).to.be.equal(skip)
-      expect(contextMustBeObject(null, null, () => {})).to.be.equal(skip)
-      expect(
-        // eslint-disable-next-line no-new-wrappers
-        contextMustBeObject(null, null, new String('string object'))
-      ).to.be.equal(skip)
+      // eslint-disable-next-line no-new-wrappers
+      const stringObject = new String('string object')
+      expect(contextMustBeObject(null, null, {})).toEqual(skip)
+      expect(contextMustBeObject(null, null, [])).toEqual(skip)
+      expect(contextMustBeObject(null, null, () => {})).toEqual(skip)
+      expect(contextMustBeObject(null, null, stringObject)).toEqual(skip)
     })
   })
 })
