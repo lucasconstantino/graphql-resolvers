812158c0b06a826dc8282930ff1b0acdbfea1a2a
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-06-02 17:39:50 -0300

    test: updated allResolvers for jest-chain

--- test/allResolvers.test.js
+++ test/allResolvers.test.js
@@ -9,25 +9,30 @@ describe('allResolvers', () => {
 
   beforeEach(jest.clearAllMocks)
 
-  it('should resolve to an empty array when resolvers array is empty', () =>
-    expect(allResolvers([])()).resolves.toEqual([]))
+  it('should resolve to an empty array when resolvers array is empty', () => {
+    expect(allResolvers([])()).resolves.toEqual([])
+  })
 
-  it('should resolve a single resolver', () =>
-    expect(allResolvers([stringResolver])()).resolves.toEqual(['string']))
+  it('should resolve a single resolver', () => {
+    expect(allResolvers([stringResolver])()).resolves.toEqual(['string'])
+  })
 
-  it('should resolve multiple resolvers', () =>
+  it('should resolve multiple resolvers', () => {
     expect(allResolvers([stringResolver, numberResolver])()).resolves.toEqual([
       'string',
       2
-    ]))
+    ])
+  })
 
-  it('should resolve composed resolvers', () =>
+  it('should resolve composed resolvers', () => {
     expect(
       allResolvers([stringResolver, numberResolver, pipedResolver])()
-    ).resolves.toEqual(['string', 2, 2]))
+    ).resolves.toEqual(['string', 2, 2])
+  })
 
-  it('should throw when no argument is provided', () =>
-    expect(() => allResolvers()()).toThrow())
+  it('should throw when no argument is provided', () => {
+    expect(() => allResolvers()()).toThrow()
+  })
 
   it('should pass any argument to the resolvers', async () => {
     await allResolvers([stringResolver, numberResolver])(1, 2, 3, 4, 5)
